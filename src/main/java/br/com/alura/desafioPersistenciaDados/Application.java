5ef62087cbb3c66f885af45c3beac647fef36a8b
Nildson B Andrade <bezerranildson@gmail.com>
2026-09-03 22:18:04 -0300

    Configuração de aplication.propeties e main

--- src/main/java/br/com/alura/desafioPersistenciaDados/Application.java
+++ src/main/java/br/com/alura/desafioPersistenciaDados/Application.java
@@ -1,11 +1,21 @@
 package br.com.alura.desafioPersistenciaDados;
 
+import br.com.alura.desafioPersistenciaDados.principal.Menu;
+import br.com.alura.desafioPersistenciaDados.repository.ArtistaRepository;
+import br.com.alura.desafioPersistenciaDados.repository.MusicaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.security.Principal;
+
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+	@Autowired
+	private ArtistaRepository artistaRepository;
+	@Autowired
+	private MusicaRepository musicaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -13,6 +23,7 @@ public class Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		Menu menu = new Menu(artistaRepository, musicaRepository);
+		menu.exibirMenu();
 	}
 }
